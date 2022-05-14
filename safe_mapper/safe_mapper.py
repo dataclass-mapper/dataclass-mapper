8ba84d0eb3182c53b2df674ffa462ce6bd6e2ec9
Jakob Kogler <jakob.kogler@gmail.com>
2022-05-14 13:29:23 +0200

    refactor common parts into a class

--- safe_mapper/safe_mapper.py
+++ safe_mapper/safe_mapper.py
@@ -1,28 +1,62 @@
-from dataclasses import fields
+from dataclasses import Field, fields
 from importlib import import_module
 from typing import Any, Callable, Optional, Type, TypeVar, cast
 
 
-def _make_mapper(mapping: dict[str, str], source_cls: Any, target_cls: Any) -> str:
-    lines = [
-        f"def convert(self):",
-        f"    d = {{}}",
-    ]
+class MappingFunction:
+    def __init__(
+        self,
+        target_cls: Any,
+        actual_source_fields: dict[str, Field],
+        actual_target_fields: dict[str, Field],
+    ):
+        self.target_cls_name = target_cls.__name__
+        self.actual_source_fields = actual_source_fields
+        self.actual_target_fields = actual_target_fields
+        self.lines = [
+            f"def convert(self):",
+            f"    d = {{}}",
+        ]
 
+    def _add_line(self, left_side: str, right_side: str) -> None:
+        self.lines.append(f'    d["{left_side}"] = {right_side}')
+
+    def add_assignment(self, target_field_name: str, source_field_name: str) -> None:
+        self._add_line(target_field_name, f"self.{source_field_name}")
+
+    def add_recursive(
+        self, target_field_name: str, source_field_name: str, target_cls: Any
+    ) -> None:
+        self._add_line(
+            target_field_name, f"map_to(self.{source_field_name}, {target_cls.__name__})"
+        )
+
+    def add_mapping(self, target_field_name: str, source_field_name: str) -> None:
+        source_type = self.actual_source_fields[source_field_name].type
+        target_type = self.actual_target_fields[target_field_name].type
+        if is_mappable_to(source_type, target_type):
+            self.add_recursive(target_field_name, source_field_name, target_type)
+        else:
+            self.add_assignment(target_field_name, source_field_name)
+
+    def __str__(self) -> str:
+        return_statement = f"    return {self.target_cls_name}(**d)"
+        return "\n".join(self.lines + [return_statement])
+
+
+def _make_mapper(mapping: dict[str, str], source_cls: Any, target_cls: Any) -> str:
     actual_source_fields = {field.name: field for field in fields(source_cls)}
     actual_target_fields = {field.name: field for field in fields(target_cls)}
+    mapping_func = MappingFunction(
+        target_cls,
+        actual_source_fields=actual_source_fields,
+        actual_target_fields=actual_target_fields,
+    )
     mapped_target_fields = set(mapping)
 
     for target_field, source_field in mapping.items():
         if target_field in actual_target_fields:
-            source_type = actual_source_fields[source_field].type
-            target_type = actual_target_fields[target_field].type
-            if is_mappable_to(source_type, target_type):
-                lines.append(
-                    f'    d["{target_field}"] = map_to(self.{source_field}, {target_type.__name__})'
-                )
-            else:
-                lines.append(f'    d["{target_field}"] = self.{source_field}')
+            mapping_func.add_mapping(target_field, source_field)
         else:
             raise ValueError(
                 f"'{target_field}' of mapping in '{source_cls.__name__}' doesn't exist in '{target_cls.__name__}'"
@@ -31,19 +65,13 @@ def _make_mapper(mapping: dict[str, str], source_cls: Any, target_cls: Any) -> s
     # handle missing fields
     for field in actual_target_fields.keys() - mapped_target_fields:
         if field in actual_source_fields:
-            source_type = actual_source_fields[field].type
-            target_type = actual_target_fields[field].type
-            if is_mappable_to(source_type, target_type):
-                lines.append(f'    d["{field}"] = map_to(self.{field}, {target_type.__name__})')
-            else:
-                lines.append(f'    d["{field}"] = self.{field}')
+            mapping_func.add_mapping(field, field)
         else:
             raise ValueError(
                 f"'{field}' of '{target_cls.__name__}' has no mapping in '{source_cls.__name__}'"
             )
 
-    lines.append(f"    return {target_cls.__name__}(**d)")
-    return "\n".join(lines)
+    return str(mapping_func)
 
 
 def get_map_to_func_name(cls: Any) -> str:
