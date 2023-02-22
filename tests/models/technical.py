bd300e28821f1260e15ebb09cc676ec947a44a24
Marina <maaaaaaaaaaaaaaaaaaaaar@gmail.com>
2023-02-22 15:59:43 +0100

    change test model import path to absolute

--- tests/models/technical.py
+++ tests/models/technical.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from dataclass_mapper.mapper import mapper_from
-from .base import SongData as BaseSong
+from tests.models.base import SongData as BaseSong
 
 
 @mapper_from(BaseSong)
